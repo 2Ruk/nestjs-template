22d44993763eb13623887fa9b843f5ff6d928133
2Ruk <leerukdev@gmail.com>
2022-07-13 10:26:08 +0900

    init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {ShareLibraryModule} from "@app/share-library";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
+import { ConnectModule } from './connect/connect.module';
 
 @Module({
-  imports: [],
+  imports: [ShareLibraryModule, ConnectModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
+
 export class AppModule {}
